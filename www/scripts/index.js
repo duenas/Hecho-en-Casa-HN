87369744a87368117e2cf82664b075eeee138ec3
roy david dueñas alvarez <roy_david0701@hotmail.com>
2018-03-13 22:11:44 -0600

    Initial commit

---
+++ www/scripts/index.js
@@ -0,0 +1,47 @@
+﻿// Si quiere una introducción sobre la plantilla En blanco, vea la siguiente documentación:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// Para depurar código al cargar la página en cordova-simulate o en dispositivos o emuladores Android: inicie la aplicación, establezca puntos de interrupción 
+// y ejecute "window.location.reload()" en la Consola de JavaScript.
+(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Controlar la pausa de Cordova y reanudar eventos
+
+       
+
+        document.getElementById("inicio").addEventListener('click', BuscarUsuario, false);
+
+
+
+
+
+
+
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+        
+      
+    };
+
+    function onPause() {
+        // TODO: esta aplicación se ha suspendido. Guarde el estado de la aplicación aquí.
+    };
+
+    function onResume() {
+        // TODO: esta aplicación se ha reactivado. Restaure el estado de la aplicación aquí.
+    };
+
+
+    function BuscarUsuario() {
+        alert("Bienvenido");
+        window.location.href = 'Menu.html';
+    }
+
+
+
+
+
+} )();
