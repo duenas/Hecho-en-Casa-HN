8719508845a06ef722165956d09985f6b0baa11c
roy david dueñas alvarez <roy_david0701@hotmail.com>
2018-04-11 23:07:31 -0600

    llistaItem

---
+++ www/scripts/ListaItems.js
@@ -0,0 +1,60 @@
+﻿// Si quiere una introducción sobre la plantilla En blanco, vea la siguiente documentación:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// Para depurar código al cargar la página en cordova-simulate o en dispositivos o emuladores Android: inicie la aplicación, establezca puntos de interrupción 
+// y ejecute "window.location.reload()" en la Consola de JavaScript.
+(function () {
+    "use strict";
+
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+    alert("lista");
+
+    var valores = getGET();
+
+    var Catego = valores['categoria'];
+
+    alert(Catego);
+
+    function getGET() {
+        // capturamos la url
+        var loc = document.location.href;
+        // si existe el interrogante
+        if (loc.indexOf('?') > 0) {
+            // cogemos la parte de la url que hay despues del interrogante
+            var getString = loc.split('?')[1];
+            // obtenemos un array con cada clave=valor
+            var GET = getString.split('&');
+            var get = {};
+
+            // recorremos todo el array de valores
+            for (var i = 0, l = GET.length; i < l; i++) {
+                var tmp = GET[i].split('=');
+                get[tmp[0]] = unescape(decodeURI(tmp[1]));
+            }
+            return get;
+        }
+    }
+
+
+    function onDeviceReady() {
+       
+       
+        var prodId = getParameterByName('categoria');
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+    };
+
+    function onPause() {
+        // TODO: esta aplicación se ha suspendido. Guarde el estado de la aplicación aquí.
+    };
+
+    function onResume() {
+        // TODO: esta aplicación se ha reactivado. Restaure el estado de la aplicación aquí.
+    };
+
+
+   
+
+
+
+
+})();
--- www/scripts/Menu.js
+++ www/scripts/Menu.js
@@ -28,8 +28,10 @@
         //var cadena = "<div class='row'>";
         //cadena = cadena + ""
         var cadena = "";
+
        
         //agregando evento Ajax
+        var cadena1 = "";
         $.ajax({
             type: "GET",
             url: "http://localhost:8585/PostCategorias.aspx",
@@ -40,8 +42,33 @@
             dataType: "json",
             success: function (result) {
                 $.each(result, function (i, field) {
+                    cadena = cadena + "<div class='row'>";
+                    cadena = cadena + "<div class='col-md-3'>";
+                    cadena = cadena + "<div class='ibox'>";
+                    cadena = cadena + "<div class='ibox-content product-box'>";
+                    cadena = cadena + "<div>";
                     cadena = cadena + "<img class='border-top' width='300' height='250' src='" + field.direccion + "'/>";
-                    //cadena = "<div class='row'> <div class='col-md-3'> <div class='ibox'> <div class='ibox-content product-box'> <div> <img class='border- top' width='300' height='250' src='" + field.direccion + "/>  </div>  <div class='product- desc'> <a href='#' class='product- name'> Zapatos de Hombre</a> <div class='small m-t-xs'> Todo tipo de Zapato para Hombre </div> <div class='m-t text- righ'> <a href='ListaItems.html' class='btn btn- xs btn- outline btn- primary'>Ir <i class='fa fa- long - arrow - right'></i> </a>  </div> </div> </div> </div> </div> </div> " 
+                    cadena = cadena + "</div>";
+                    cadena = cadena + "<div class='product-desc'>";
+                    cadena = cadena + "<a href='#' class='product-name'>" + field.nombre +"</a>";
+                    cadena = cadena + "<div class='small m-t-xs'>";
+                    cadena = cadena + field.descripcion;
+                    cadena = cadena + "</div>";
+                    cadena = cadena + "<div class='m-t text-righ'>";
+                    cadena = cadena + "<a href='ListaItems.html?categoria=" + field.id + "' class='btn btn-xs btn-outline btn-primary'>Ir <i class='fa fa-long-arrow-right'></i> </a>";
+                    cadena = cadena + "</div>";
+                    cadena = cadena + "</div>";
+                    cadena = cadena + "</div>";
+                    cadena = cadena + "</div>";
+                    cadena = cadena + "</div>";
+                    cadena = cadena + "</div>";
+
+
+
+
+                    ////cadena1 = cadena1 + field.nombre;
+                    ////cadena1 = cadena1 +  field.id;
+                    //cadena = "<div class='row'> <div class='col-md-3'> <div class='ibox'> <div class='ibox-content product-box'> <div> <img class='border- top' width='300' height='250' src='" + field.direccion + "/>  </div>  <div class='product- desc'> <a href='#' class='product- name'> Zapatos de Hombre</a> <div class='small m-t-xs'> Todo tipo de Zapato para Hombre </div> <div class='m-t text- righ'> <a href='ListaItems.html' class='btn btn- xs btn- outline btn- primary'>Ir <i class='fa fa- long - arrow - right'></i> </a>  </div> </div> </div> </div> </div> </div> "
                     //cadena = cadena + " <tr >" + "<td data-edicion=" + field.edicion + "class=center-wrapper ><h3 class= 'center-wrapper'>" + field.edicion + "</h3>" + " <img  align=middle border=5 style=display: block;  width=300 height=250 src=" + field.urlportada + ">" + "" + " </td>" + " </tr>";
 
                 });
